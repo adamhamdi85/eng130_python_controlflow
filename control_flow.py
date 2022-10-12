74f720337bc78bf38160c6cb8ded4893561b0fd5
adamhamdi85 <adamhamdi85@gmail.com>
2022-10-12 14:57:01 +0100

    loops - elif - while

--- control_flow.py
+++ control_flow.py
@@ -1,29 +1,36 @@
 # sudo coding
+#
+# weather = "time"
 
-weather = "time"
-
-# if it's cold:
-if weather == "cold": # true or false next line executed
-    print("wear a jacket")
-     #take jacket
-elif weather == "sunny":
-    print("let's go to the beach")
-
-# if it's raining:
-else:
- print("no match found better luck next time")
-     #rain mack
-
-# if it's sunny: boolean value true - next line
-     #let's go to the beach
-# else or elif
+# # if it's cold:
+# if weather == "cold": # true or false next line executed
+#     print("wear a jacket")
+#      #take jacket
+# elif weather == "sunny":
+#     print("let's go to the beach")
+#
+# # if it's raining:
+# else:
+#  print("no match found better luck next time")
+#      #rain mack
+#
+# # if it's sunny: boolean value true - next line
+#      #let's go to the beach
+# # else or elif
 
 
 # age restriction for movie tickets
 # create a condition for 18 and above
+
+if age >= "18":
+    print("allow access")
 # 16 and above
+# if age >= "16":
 #pg
 #12a
 # 15 and above
 # if nothing matched inform the user to enter their age again
 # user must not be allowed to enter age over 117 years
+while age >117
+    print(f"")
+    data
---
+++ loops.py
@@ -0,0 +1,26 @@
+list_data = [1, 2, 3, 4, 5]
+for number in list_data:
+    if number == 3:
+       print(number)
+    elif number > 3:
+        print(" you have passed 3")
+    else:
+        print("too soon")
+
+# create a dictionary student_data
+# iterate through the dict
+# using control flow - if elif - else and for loop print all the keys
+# print key with matching value
+
+
+
+
+
+
+
+# print(list_data)
+# print(list_data[0])
+# print(list_data[1])
+# print(list_data[2])
+# print(list_data[3])
+# print(list_data[4])
